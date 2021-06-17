1e70acd6d9570d422783b8b7de312c3e6e46264f
NIMROD GABBAY <nimrod240597@gmail.com>
2021-06-17 15:31:53 +0300

    commit

--- app/src/main/java/com/example/flightandroidapp/views/Joystick.java
+++ app/src/main/java/com/example/flightandroidapp/views/Joystick.java
@@ -1,58 +1,122 @@
 package com.example.flightandroidapp.views;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.drawable.ShapeDrawable;
-import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
-import com.example.flightandroidapp.R;
+import androidx.databinding.Observable;
 
-import java.util.jar.Attributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observer;
 
-public class Joystick extends AppCompatActivity{
+public class Joystick extends View {
 
-    private double elevator;
-    private double ailerone;
+    private float x;
+    private float y;
+    private float defaultX;
+    private float defaultY;
     private float dx;
     private float dy;
-    private View.OnTouchListener touchListener;
+    private float radius;
+    private Paint paint;
+    private boolean moveJoystick;
+    public OnJoystickChange onChange;
+
+
+    public Joystick(Context context) {
+        super(context);
+//        paint = new Paint();
+//        paint.setColor(Color.BLUE);
+//        this.moveJoystick = false;
+//        this.radius = 150;
+//        this.defaultX=0;
+//        this.defaultY=0;
+    }
+
+    public Joystick(Context context, AttributeSet attrs){
+        super(context,attrs);
+        paint = new Paint();
+        paint.setColor(Color.BLUE);
+        this.moveJoystick = false;
+        this.radius = 150;
+        this.defaultX=0;
+        this.defaultY=0;
+        this.x=defaultX;
+        this.y=defaultY;
+    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_joystick);
-        touchListener=new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                switch (motionEvent.getActionMasked()){
-                    case MotionEvent.ACTION_DOWN:
-                        dx=view.getX()-motionEvent.getRawX();
-                        dy= view.getY()-motionEvent.getRawY();
-                        break;
-                    case MotionEvent.ACTION_MOVE:
-                        view.setX(motionEvent.getRawX()+dx);
-                        view.setY(motionEvent.getRawY()+dy);
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        break;
-                    default:
-                        return false;
-                }
-                return true;
-            }
-
-        };
-        final View joystick=findViewById(R.id.circle);
-        joystick.setOnTouchListener(this.touchListener);
-
+    protected void onDraw(Canvas canvas) {
+        if(defaultX==0&&defaultY==0){
+            defaultX=getWidth()/2;
+            defaultY=getHeight()/2+300;
+            backToDefaultAxes();
+        }
+        canvas.drawCircle(x,y,this.radius,paint);
     }
+    private void backToDefaultAxes(){
+        this.x=defaultX;
+        this.y=defaultY;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float newX=event.getX();
+        float newY=event.getY();
+        switch (event.getActionMasked()) {
+            case MotionEvent.ACTION_DOWN:
+                if(isInside(newX,newY))
+                    moveJoystick=true;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if(!moveJoystick)
+                    return true;
+                dx=newX-x;
+                dy=newY-y;
+                if(dx>100)
+                    dx=100;
+                else if(dx<-100)
+                    dx=-100;
+                if(dy>100)
+                    dy=100;
+                else if(dy<-100)
+                    dy=-100;
+                this.x+=dx;
+                this.y+=dy;
+                onChange=(dx,dy)->{
+                  dx=dx/100;
+                  dy=dy/100;
+                };
+                invalidate();
+
+                break;
+            case MotionEvent.ACTION_UP:
+                moveJoystick=false;
+                backToDefaultAxes();
+                invalidate();
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
+    private boolean isInside(float xVal,float yVal){
+        return Math.sqrt((this.x-xVal)*(this.x-xVal) + (this.y-yVal)*(this.y-yVal))<=this.radius;
+    }
+
+    public float getDx() {
+        return dx;
+    }
+
+    public float getDy() {
+        return dy;
+    }
+
 
 }
--- app/src/main/java/com/example/flightandroidapp/views/MainActivity.kt
+++ app/src/main/java/com/example/flightandroidapp/views/MainActivity.kt
@@ -5,33 +5,44 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.SeekBar
+import androidx.databinding.DataBindingUtil
 import com.example.flightandroidapp.R
-import com.example.flightandroidapp.model.FGPlayer
+import com.example.flightandroidapp.databinding.ActivityMainBinding
 import com.example.flightandroidapp.view_model.ViewModel
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Observer {
 
-    open var viewModel: ViewModel? =null
-    private var joystick: Joystick?=null
-    private var throttle: SeekBar?= null
-    private var rudder: SeekBar? =null
+    open var viewModel: ViewModel? = null
+
+    //    private var joystick: Joystick?=null
+    private var throttle: SeekBar? = null
+    private var rudder: SeekBar? = null
+    private var ailerone: Float = 0F
+    private var elevator: Float = 0F
+    private var onJoystickChange: OnJoystickChange? = null
+
+    //    private var touchListener :View.OnTouchListener?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        setContentView(R.layout.activity_joystick)
-        viewModel= ViewModel()
-        joystick=Joystick()
-        throttle=findViewById<SeekBar>(R.id.throttle)
-        rudder=findViewById<SeekBar>(R.id.rudder)
-        throttle?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+        viewModel = ViewModel()
+//        joystick=Joystick(this)
+//        binding=DataBindingUtil.setContentView<>(this,R.layout.activity_main)
+        throttle = findViewById<SeekBar>(R.id.throttle)
+        rudder = findViewById<SeekBar>(R.id.rudder)
+
+        throttle?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seek: SeekBar?, p1: Int, p2: Boolean) {
-                val old_val=p1
-                val old_min=0
-                val old_max=100
-                val new_min=0
-                val new_max=1
-                val new_val: Double
-                new_val= (((old_val-old_min)/(old_max-old_min)) * (new_max-new_min) +new_min).toDouble()
+                val old_val = p1
+                val old_min = 0
+                val old_max = 100
+                val new_min = 0
+                val new_max = 1
+                val new_val: Float
+                new_val =
+                    (((old_val - old_min) / (old_max - old_min)) * (new_max - new_min) + new_min).toFloat()
                 viewModel!!.setVM_throttle(new_val)
             }
 
@@ -43,15 +54,16 @@ class MainActivity : AppCompatActivity() {
                 TODO("Not yet implemented")
             }
         })
-        rudder?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+        rudder?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seek: SeekBar?, p1: Int, p2: Boolean) {
-                val old_val=p1
-                val old_min=0
-                val old_max=100
-                val new_min=-1
-                val new_max=1
-                val new_val: Double
-                new_val= (((old_val-old_min)/(old_max-old_min)) * (new_max-new_min) +new_min).toDouble()
+                val old_val = p1
+                val old_min = 0
+                val old_max = 100
+                val new_min = -1
+                val new_max = 1
+                val new_val: Float
+                new_val =
+                    (((old_val - old_min) / (old_max - old_min)) * (new_max - new_min) + new_min).toFloat()
                 viewModel!!.setVM_rudder(new_val)
             }
 
@@ -63,15 +75,52 @@ class MainActivity : AppCompatActivity() {
                 TODO("Not yet implemented")
             }
         })
-//        joystick= Joystick(this)
-//        setContentView(joystick)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main
+        )
+//        joystick.onTouchEvent(event: MotionEvent?)
+//        ailerone = binding.joystick.dx / 100
+//        elevator = binding.joystick.dy / 100
+//        viewModel?.setVM_ailerone(ailerone)
+//        viewModel?.setVM_el(elevator)
+        var joystick = Joystick(this)
+        joystick.onChange = OnJoystickChange { a: Float, e: Float ->
+            viewModel!!.setVM_ailerone(a)
+            viewModel!!.setVM_elevator(e)
 
+        }
     }
-    fun onClickFunc(view: View){
-        val ip=findViewById<EditText>(R.id.ip)
-        val port=findViewById<EditText>(R.id.port)
-        viewModel?.VM_connect(ip.text.toString(),port.text.toString())
+
+    fun onClickFunc(view: View) {
+        val ip = findViewById<EditText>(R.id.ip)
+        val port = findViewById<EditText>(R.id.port)
+        viewModel?.VM_connect(ip.text.toString(), port.text.toString())
     }
+
+    override fun onDestroy() {
+        viewModel?.VM_destroy()
+        super.onDestroy()
+    }
+
+//    fun onChange(joystick: Joystick){
+//
+//    }
+
+    override fun update(p0: Observable?, p1: Any?) {
+        TODO("Not yet implemented")
+    }
+//    override fun onTouchEvent(event: MotionEvent?): Boolean {
+//        joystick?.onTouchEvent(event)
+//        val xPos= joystick?.getDx()
+//        val yPos= joystick?.getDy()
+//        if (xPos != null) {
+//            viewModel?.setVM_ailerone(xPos/100)
+//        }
+//        if (yPos != null) {
+//            viewModel?.setVM_elevator(yPos/100)
+//        }
+//        return true;
+//    }
 
 
 }
